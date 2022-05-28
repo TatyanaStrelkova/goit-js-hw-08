46a9d6b6af5ed1b6f91e0300a24907bea18a182c
Tatyana <tatyana_strelkova@i.ua>
2022-05-28 19:39:05 +0300

    add third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    message: document.querySelector('textarea'),
+} 
+
+const STORAGE_KEY = 'feedback-form-state';
+
+fillTextArea()
+
+function onFormInput(e) { 
+    const formData = {
+        email: e.currentTarget.elements.email.value,
+        message: e.currentTarget.elements.message.value
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+} 
+
+function fillTextArea() { 
+    const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedFormData) { 
+        refs.input.value = savedFormData.email;
+        refs.message.value = savedFormData.message;
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();    
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+ }
+
+refs.form.addEventListener('input', throttle(onFormInput, 500))
+refs.form.addEventListener('submit', onFormSubmit)
